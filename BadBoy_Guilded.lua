55871ed830917b203e0521214ea03aa93f4e6398
funkydude <funkydude@users.noreply.github.com>
2010-12-24 01:54:38 +0000

    remove debug print

--- BadBoy_Guilded.lua
+++ BadBoy_Guilded.lua
@@ -15,7 +15,6 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_,_,msg,player,_,_,
 	for i = 1, 5 do
 		if (msg):find(triggers[i]) then --Found a match
 			result = true
-			if BADBOY_DEBUG then print("|cFF33FF99BadBoy_GUILDED_BLOCK|r: ", msg) end
 			return true --found a trigger, filter
 		end
 	end
