cd2d4773a9ea5267ca28d9e1771242f8f7934632
funkydude <funkydude@users.noreply.github.com>
2015-01-11 14:06:48 +0000

    filter more ads

--- BadBoy_Guilded.lua
+++ BadBoy_Guilded.lua
@@ -91,6 +91,7 @@ local prevLineId, result, triggers = 0, nil, {
 	"guild.*mature.*social", -- -*- is a large social guild with lots of friendly mature people, who enjoy the social side of the game we do classic raids, achies, rep runs if youre after a friendly social guild look no further than * and give me a wisper  :)
 	"looking.*community.*accepting", --<*> Are looking for more people. We are creating a mature and active community to participate in dungeon runs and casual raiding in WoD. Accepting all classes, whisper for more info or an invite.
 	"team.*looking.*people", --<*> LF Healer   for our raiding team,We are looking for people with excellent communication skills whom are willing to put an effort into raiding with us!
+	"looking.*progress.*/w", --<*> (7/7N 2/7HC) Is looking for skilled DPS + Healers for our progression to close out highmaul and move further into HC. iLvl is no prerequisite, we are looking for loyalty and potential. Sound like your kind of environment? Give me a /w!
 
 	--Dutch
 	"guild.*zoek naar.*social", -- [25] Nederlands sprekende Guild <*> zijn op zoek naar Tanks: Geen / Melee dps: Warrior / Ranger dps: warlock, Mage / Healers: Paladin / raid tijden ma, di ,do van 20:00ST tot 23:00ST, social invite is ook mogelijk whisper voor meer info.
