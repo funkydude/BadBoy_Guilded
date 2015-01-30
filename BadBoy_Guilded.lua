3494fa67fdad9f41f894db47c13a6754e16a5d93
funkydude <funkydude@users.noreply.github.com>
2015-01-30 22:30:07 +0000

    filter more ads

--- BadBoy_Guilded.lua
+++ BadBoy_Guilded.lua
@@ -93,6 +93,8 @@ local prevLineId, result, triggers = 0, nil, {
 	"team.*looking.*people", --<*> LF Healer   for our raiding team,We are looking for people with excellent communication skills whom are willing to put an effort into raiding with us!
 	"looking.*progress.*/w", --<*> (7/7N 2/7HC) Is looking for skilled DPS + Healers for our progression to close out highmaul and move further into HC. iLvl is no prerequisite, we are looking for loyalty and potential. Sound like your kind of environment? Give me a /w!
 	"team.*social.*welcome", --<*> Lfm for fresh raid team starting with highmaul normal , ts3 , mondays at 8:30pm to 10:30pm realm time. socials also welcome  ,whisper me for info/invite.
+	--* is looking for more experienced and willing DPS and a tank or healer for our second raid team, your dps and movement needs to be brilliant:) All we ask is mature and positive behaviour. Please /w me if interested
+	"looking.*experi[ae]nced.*raid", --"*" 7/7 N - 6/7 HC. is curently Looking for more raid experianced ppl For moving Mythic were kinda low on warlocks and a good boomkin might do wonders :D We raid Mon/Tues and Thursdays 19.30 server time. Have a nice game :)
 
 	--Dutch
 	"guild.*zoek naar.*social", -- [25] Nederlands sprekende Guild <*> zijn op zoek naar Tanks: Geen / Melee dps: Warrior / Ranger dps: warlock, Mage / Healers: Paladin / raid tijden ma, di ,do van 20:00ST tot 23:00ST, social invite is ook mogelijk whisper voor meer info.
