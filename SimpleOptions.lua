ed6d2a44e27327ffae451863a6c6ce7de856bf8d
funkydude <funkydude@users.noreply.github.com>
2015-03-10 11:58:54 +0000

    filter more ads

--- SimpleOptions.lua
+++ SimpleOptions.lua
@@ -30,13 +30,13 @@ do
 	elseif L == "zhTW" then
 		guildedWhispersText:SetText("Remove guild invite whispers")
 	elseif L == "zhCN" then
-		guildedWhispersText:SetText("Remove guild invite whispers")
+		guildedWhispersText:SetText("移除公会邀请密语")
 	elseif L == "esES" then
 		guildedWhispersText:SetText("Remove guild invite whispers")
 	elseif L == "esMX" then
 		guildedWhispersText:SetText("Remove guild invite whispers")
 	elseif L == "ruRU" then
-		guildedWhispersText:SetText("Remove guild invite whispers")
+		guildedWhispersText:SetText("Блокировать личные сообщения, содержащие приглашения в гильдию")
 	elseif L == "koKR" then
 		guildedWhispersText:SetText("Remove guild invite whispers")
 	elseif L == "ptBR" then
