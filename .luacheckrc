c9cc3076db75cd8640cb7f20310c706606e81f5b
funkydude <funkydude@users.noreply.github.com>
2021-05-26 21:36:06 +0100

    Fix luacheck issues

--- .luacheckrc
+++ .luacheckrc
@@ -2,5 +2,20 @@ std = "lua51"
 max_line_length = false
 codes = true
 globals = {
+	-- Main
+	"Ambiguate",
+	"BADBOY_GWHISPER",
+	"BadBoyIsFriendly",
+	"BadBoyLog",
+	"ChatFrame_AddMessageEventFilter",
+	"IsInGuild",
+
+	-- Options
+	"BadBoyConfig",
+	"BadBoyGuildedConfigTitle",
 	"CreateFrame",
+	"GetAutoDeclineGuildInvites",
+	"GetLocale",
+	"PlaySound",
+	"SetAutoDeclineGuildInvites",
 }
--- SimpleOptions.lua
+++ SimpleOptions.lua
@@ -21,27 +21,40 @@ do
 	guildedWhispersText:SetPoint("LEFT", guildedWhispers, "RIGHT", 0, 1)
 
 	guildedWhispersText:SetText("Remove guild invite whispers")
-	local L = GetLocale()
-	if L == "frFR" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
-	elseif L == "deDE" then
-		guildedWhispersText:SetText("Entferne geflüsterte Gildeneinladungen")
-	elseif L == "zhTW" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
-	elseif L == "zhCN" then
-		guildedWhispersText:SetText("移除公会邀请密语")
-	elseif L == "esES" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
-	elseif L == "esMX" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
-	elseif L == "ruRU" then
-		guildedWhispersText:SetText("Блокировать личные сообщения, содержащие приглашения в гильдию")
-	elseif L == "koKR" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
-	elseif L == "ptBR" then
-		guildedWhispersText:SetText("Remove sussurros de convites de guilda")
-	elseif L == "itIT" then
-		--guildedWhispersText:SetText("Remove guild invite whispers")
+	local BLOCK_GUILD_INVITES = "Block Guild Invites"
+	do
+		local L = GetLocale()
+		if L == "frFR" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "Bloquer les invitations de guilde"
+		elseif L == "deDE" then
+			guildedWhispersText:SetText("Entferne geflüsterte Gildeneinladungen")
+			BLOCK_GUILD_INVITES = "Gildeneinladungen blockieren"
+		elseif L == "zhTW" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "阻止公會邀請"
+		elseif L == "zhCN" then
+			guildedWhispersText:SetText("移除公会邀请密语")
+			BLOCK_GUILD_INVITES = "阻止公会邀请"
+		elseif L == "esES" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "Bloquear invitaciones de hermandad"
+		elseif L == "esMX" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "Bloquear invitaciones de hermandad"
+		elseif L == "ruRU" then
+			guildedWhispersText:SetText("Блокировать личные сообщения, содержащие приглашения в гильдию")
+			BLOCK_GUILD_INVITES = "Блокировать приглашения в гильдию"
+		elseif L == "koKR" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "길드 초대 차단"
+		elseif L == "ptBR" then
+			guildedWhispersText:SetText("Remove sussurros de convites de guilda")
+			BLOCK_GUILD_INVITES = "Bloquear convites da guilda"
+		elseif L == "itIT" then
+			--guildedWhispersText:SetText("Remove guild invite whispers")
+			BLOCK_GUILD_INVITES = "Blocca inviti di gilda"
+		end
 	end
 
 	local guildedInvites = CreateFrame("CheckButton", nil, BadBoyConfig, "OptionsBaseCheckButtonTemplate")
